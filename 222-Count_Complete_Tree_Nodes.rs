d83836b66acef98e3671dd6326cb45d85d0c1629
そのだ　かずと <86616409+SonodaKazuto@users.noreply.github.com>
2023-09-23 13:46:54 +0900

    submit(Rust): LeetCode 222. Count Complete Tree Nodes
    Signed-off-by: そのだ　かずと <86616409+SonodaKazuto@users.noreply.github.com>

---
+++ 222-Count_Complete_Tree_Nodes.rs
@@ -0,0 +1,32 @@
+// Definition for a binary tree node.
+// #[derive(Debug, PartialEq, Eq)]
+// pub struct TreeNode {
+//   pub val: i32,
+//   pub left: Option<Rc<RefCell<TreeNode>>>,
+//   pub right: Option<Rc<RefCell<TreeNode>>>,
+// }
+// 
+// impl TreeNode {
+//   #[inline]
+//   pub fn new(val: i32) -> Self {
+//     TreeNode {
+//       val,
+//       left: None,
+//       right: None
+//     }
+//   }
+// }
+use std::rc::Rc;
+use std::cell::RefCell;
+impl Solution {
+    pub fn count_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        if let Some(ref node) = root {
+            let left_tree = node.borrow().left.clone();
+            let right_tree = node.borrow().right.clone();
+
+            return 1 + Solution::count_nodes(left_tree) + Solution::count_nodes(right_tree);
+        }
+        return 0;
+    }
+
+}
